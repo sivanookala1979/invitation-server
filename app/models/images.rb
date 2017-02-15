d88e4124bf61876eb20d6687ad1a826c998728d4
rakul <siva.nookala@gmail.com>
2017-02-15 16:00:10 +0530

    1.modified call.

--- app/models/images.rb
+++ app/models/images.rb
@@ -1,2 +1,9 @@
 class Images < ActiveRecord::Base
+  has_attached_file :image_path, :styles => {:small => "350x350>", :large=>"650x650>"},
+                    :url  => "/assets/image/:id/:style/:basename.:extension",
+                    :path => ":rails_root/public/assets/image/:id/:style/:basename.:extension"
+
+  validates_attachment_presence :image_path
+  validates_attachment_size :image_path, :less_than => 5.megabytes
+  validates_attachment_content_type :image_path, :content_type => ['image/jpeg', 'image/png']
 end
