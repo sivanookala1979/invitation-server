d7318098c4f0b91493e89d4fcb55cf5447d52ea7
rakul <siva.nookala@gmail.com>
2017-03-09 19:40:10 +0530

    1.add image url to events.

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -215,7 +215,7 @@ class EventsController < ApplicationController
           is_admin = @event_admin.present? ? true : false
           invitation = Invitation.find_by_event_id_and_participant_id(event.id, user.id)
           is_accepted = invitation.present? && invitation.is_accepted.present? ? invitation.is_accepted : false
-          img_url = (image = Image.find_by_id(event.image_id)).present? ? ApplicationHelper.get_root_url+image.image_path.url(:original) : ''
+          img_url = (image = Images.find_by_id(event.image_id)).present? ? ApplicationHelper.get_root_url+image.image_path.url(:original) : ''
           all_my_events << EventDetails.new(event.id.to_i,event.event_name, event.end_date, event.description, event.latitude, event.longitude, event.address, event.private, event.remainder, event.status, event.owner_id, event.start_date, event.invitees_count, event.accepted_count, event.rejected_count, event.is_manual_check_in, event.check_in_count, event.is_recurring_event, event.recurring_type, event.event_theme, is_accepted,is_admin,img_url)
         end
       else
@@ -226,7 +226,7 @@ class EventsController < ApplicationController
           is_admin = @event_admin.present? ? true : false
           invitation = Invitation.find_by_event_id_and_participant_id(event.id, user.id)
           is_accepted = invitation.present? && invitation.is_accepted.present? ? invitation.is_accepted : false
-          img_url = (image = Image.find_by_id(event.image_id)).present? ? ApplicationHelper.get_root_url+image.image_path.url(:original) : ''
+          img_url = (image = Images.find_by_id(event.image_id)).present? ? ApplicationHelper.get_root_url+image.image_path.url(:original) : ''
           all_my_events << EventDetails.new(event.id.to_i,event.event_name, event.end_date, event.description, event.latitude, event.longitude, event.address, event.private, event.remainder, event.status, event.owner_id, event.start_date, event.invitees_count, event.accepted_count, event.rejected_count, event.is_manual_check_in, event.check_in_count, event.is_recurring_event, event.recurring_type, event.event_theme, is_accepted,is_admin,img_url)
         end
       end
